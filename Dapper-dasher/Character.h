f5bd283402dc461b67cfbe1efa9f0ddb98479bd3
Simon Magnusson <simonmagnusson9205@gmail.com>
2022-08-31 11:53:20 +0200

    Adds Nebula class

--- Dapper-dasher/Character.h
+++ Dapper-dasher/Character.h
@@ -1,7 +1,12 @@
 ﻿#pragma once
+#include "AnimData.h"
 
 class Character
 {
 public:
+    Character();
+private:
+    const Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    AnimData scarfyData;
     
 };
